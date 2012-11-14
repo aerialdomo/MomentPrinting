077c60fc0a1a3d15088b92d8f5b4437404c1c5a0
aerialdomo <christinaliu26@gmail.com>
2012-11-13 16:44:19 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MomentPrinting::Application.config.secret_token = 'b8c2dc587d76b0e50ece294e6dac3059551bf30f0635d7ba7113817a09aeefbfc2c2d95499bccfd8a4a2d02906f868b08478630223f40566604cee9d93379185'
